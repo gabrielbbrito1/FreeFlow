0278fb3335ff1dbc8fdac755e8da8572d97a37e1
Gabriel Brito <gabrielbbrito_1@hotmail.com>
2025-11-24 17:18:08 -0300

    Iniciando configurações do postgres

--- backend/freeflow/settings.py
+++ backend/freeflow/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'freeflow.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'freeflow',
+        'USER': 'postgres',
+        'PASSWORD': 'sua_senha',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
