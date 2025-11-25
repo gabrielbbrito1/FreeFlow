8e48c7c3dbb916fc889944ef14d411a743781b5f
Gabriel Brito <gabrielbbrito_1@hotmail.com>
2025-11-24 22:01:24 -0300

    Model user

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(unique=True)
+
+    # Remove username and use only e-mail
+    username = None
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
