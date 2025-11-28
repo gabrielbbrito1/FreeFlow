6eb8a372a2dd36a8eb3a45efafb81a4c12800e85
Gabriel Brito <gabrielbbrito_1@hotmail.com>
2025-11-28 16:21:47 -0300

    remover secret key do settings

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,49 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Customer, Product, Order
 
 # Register your models here.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+
+    # Campos que aparecem na LISTAGEM
+    list_display = ("email", "phone", "is_staff", "is_active")
+    list_filter = ("is_staff", "is_active")
+
+    # Campo usado como busca
+    search_fields = ("email", "phone", "id")
+
+    ordering = ("email",)
+
+    # Campos ao editar um usuário
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Informações pessoais", {"fields": ("phone",)}),
+        ("Permissões", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        ("Datas importantes", {"fields": ("last_login", "date_joined")}),
+    )
+
+    # Campos ao CRIAR um usuário
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "phone", "password1", "password2", "is_staff", "is_active"),
+        }),
+    )
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ("email", "first_name", "last_name", "phone", "city", "created_at")
+    search_fields = ("email", "phone")
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "id")
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "customer", "product", "status", "total_amount", "created_at")
+    list_filter = ("status", "id")
--- backend/core/models.py
+++ backend/core/models.py
@@ -3,9 +3,36 @@ import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from phonenumber_field.modelfields import PhoneNumberField  # pyright: ignore[reportMissingImports]
+from django.contrib.auth.models import BaseUserManager
+
 
 
 # Create your models here.
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("O usuário precisa de um email")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser precisa ter is_staff=True")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser precisa ter is_superuser=True")
+
+        return self.create_user(email, password, **extra_fields)
+
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(unique=True)
@@ -13,6 +40,7 @@ class User(AbstractUser):
     phone = PhoneNumberField(null=False, blank=False, unique=True)
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+    objects = UserManager()
 
     def __str__(self):
         return self.email
@@ -60,7 +88,5 @@ class Order(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return str(self.id)
-
-
+        return f"Order {self.id} ({self.status})"
 
