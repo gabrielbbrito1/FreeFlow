c77e130cd3a876e4e4b2f89e2c3eb961740be0ba
Gabriel Brito <gabrielbbrito_1@hotmail.com>
2025-12-09 01:13:24 -0300

    correcao da url

--- backend/freeflow/urls.py
+++ backend/freeflow/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from core import views, health_check
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
-    path("healthz/", health_check),
+    path("healthz/", views.health_check),
     # ✅ Todas as rotas da API
     path('api/', include('core.urls')),
 ]
