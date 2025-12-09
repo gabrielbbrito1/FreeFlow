63958a95f067222c95f00852b83d5d958508bf1c
Gabriel Brito <gabrielbbrito_1@hotmail.com>
2025-12-08 23:12:35 -0300

    alteracoes

--- backend/freeflow/asgi.py
+++ backend/freeflow/asgi.py
@@ -8,8 +8,11 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
-
+import sys
 from django.core.asgi import get_asgi_application
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'freeflow.settings')
 
